b341b88aa973a662646a8935d46fcd8e885c2f11
Cory D. Wiles <corywiles@icloud.com>
2019-11-20 21:34:22 -0500

    Project meta
    - new class

---
+++ SpokestackRSSReader/SpokestackRSSReader/Configs/App.swift
@@ -0,0 +1,19 @@
+//
+//  App.swift
+//  SpokestackRSSReader
+//
+//  Created by Cory Wiles on 11/20/19.
+//  Copyright © 2019 Spokestack. All rights reserved.
+//
+
+import Foundation
+
+struct App {
+    
+    struct Feed {
+        
+        static let feedURL: String = "https://feeds.feedburner.com/TechCrunch/"
+        
+        static let feedTitle: String = "TechCrunch"
+    }
+}
