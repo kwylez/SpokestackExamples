fa407a015258bcdf3cab6e147d073a5f90694102
Cory D. Wiles <corywiles@icloud.com>
2019-11-23 16:52:30 -0500

    list is showing up with cards but not spacing

--- SpokestackRSSReader/SpokestackRSSReader/Views/ContentView.swift
+++ SpokestackRSSReader/SpokestackRSSReader/Views/ContentView.swift
@@ -14,23 +14,41 @@ import Combine
 struct ContentView: View {
     
     @ObservedObject var viewModel: RSSViewModel = RSSViewModel()
-    
+                    let item = RSSFeedItem(title: "Hello", link: "link", description: "my description")
     var body: some View {
         
         VStack(alignment: .leading, spacing: 0.0) {
             
             ZStack {
     
-                    Rectangle()
-                        .foregroundColor(Color("Blue"))
-                    Text("TechCrunch")
-                        .fontWeight(.bold)
-                        .font(.largeTitle)
-                        .foregroundColor(.white)
-                        .padding(.top, 30.0)
+                Rectangle()
+                    .foregroundColor(Color("Blue"))
+                Text("TechCrunch")
+                    .fontWeight(.bold)
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding(.top, 30.0)
 
             }.frame(height: 125.0)
-            Rectangle().foregroundColor(Color("Gray"))
+            ZStack {
+                Rectangle().foregroundColor(Color("Gray"))
+
+                VStack(alignment: .center, spacing: 15.0) {
+                    List {
+                        ForEach(self.viewModel.feedItems) { item in
+                            FeedCardView(feedItem: item)
+                        }
+                    }
+                    Spacer()
+                }
+                .frame(width: UIScreen.main.bounds.width * 0.95)
+                .onAppear{
+                    UITableView.appearance().separatorStyle = .none
+                    self.viewModel.activatePipeline()
+                }.onDisappear() {
+                    self.viewModel.deactivePipeline()
+                }
+            }
         }.edgesIgnoringSafeArea(.all)
     }
 }
