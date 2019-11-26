d5b3d533fad5742acfc5dc6c25dda9a66e792c9c
Cory D. Wiles <corywiles@icloud.com>
2019-11-26 11:16:52 -0500

    reciting back article headlines

--- SpokestackRSSReader/SpokestackRSSReader/View Models/RSSViewModel.swift
+++ SpokestackRSSReader/SpokestackRSSReader/View Models/RSSViewModel.swift
@@ -14,7 +14,12 @@ class RSSViewModel: ObservableObject {
 
     // MARK: Interneal (properties)
     
-    @Published private (set) var feedItems: Array<RSSFeedItem> = []
+    @Published private (set) var feedItems: Array<RSSFeedItem> = [] {
+        
+        didSet {
+            self.queuedItems = feedItems
+        }
+    }
     
     // MARK: Private (properties)
     
@@ -22,15 +27,24 @@ class RSSViewModel: ObservableObject {
     
     private var subscriptions = Set<AnyCancellable>()
     
-    private let subscriber: SpeechControllerTranscriptSubscriber = SpeechControllerTranscriptSubscriber()
+    private var shouldAnnounceWelcome: Bool = true {
+        
+        didSet {
+            
+            if !shouldAnnounceWelcome {
     
-    private var shouldAnnounceWelcome: Bool = true
+                if !self.feedItems.isEmpty {
+                    self.processHeadlines()
+                }
+            }
+        }
+    }
+    
+    private var queuedItems: Array<RSSFeedItem> = []
     
     // MARK: Initializers
     
-    init() {
-        speechController.subject.subscribe(self.subscriber)
-    }
+    init() {}
     
     // MARK: Internal (methods)
     
@@ -43,13 +57,15 @@ class RSSViewModel: ObservableObject {
         if self.shouldAnnounceWelcome {
             
             self.speechController.respond(App.welcomeMessage)
-            self.shouldAnnounceWelcome.toggle()
+            self.load()
+            
+        } else {
+            
+            self.load()
         }
-        
-        self.load()
 
 //        self.speechController.start()
-//        self.speechController.subject.sink( receiveCompletion: { [unowned self] completion in
+//        self.speechController.textPublisher.sink( receiveCompletion: { [unowned self] completion in
 //
 //            self.speechController.stop()
 //
@@ -73,6 +89,7 @@ class RSSViewModel: ObservableObject {
         
         rssController.parseFeed({feedItems in
             self.feedItems = feedItems
+            self.shouldAnnounceWelcome.toggle()
         })
     }
     
@@ -81,6 +98,47 @@ class RSSViewModel: ObservableObject {
         self.feedItems.forEach {
             self.speechController.respond($0.title)
         }
+    }
+    
+    private func processHeadlines() -> Void {
+        
+        self.speechController.itemFinishedPublisher.sink(receiveCompletion: {_ in }, receiveValue: {value in
+            
+            if self.feedItems.count == self.queuedItems.count {
+                
+                if let item: RSSFeedItem = self.queuedItems.first {
+
+                    self.queuedItems.remove(at: 0)
+                    self.speechController.respond(item.title)
+                }
+                
+            } else if !self.queuedItems.isEmpty {
+             
+                if let nextItem: RSSFeedItem = self.queuedItems.first {
+
+                    self.queuedItems.remove(at: 0)
+                    self.speechController.respond(nextItem.title)
+                }
+
+            } else {
+
+                /// All items have been read so shut everything down: TBD
+            }
+            
+        }).store(in: &self.subscriptions)
+        
+        /// Read article after 1.5 seconds
+        ///
+        /// Should timer be instance var with no auto connect?
+        /// That way after an item has finished, Start a new timer, active ASR, if found, then cancel timer, read description, shut down ASR  and then continue
+        
+//        let cancellable = Timer.publish(every: 1.5, on: RunLoop.main, in: .common)
+//        .autoconnect()
+//        .sink { receivedTimeStamp in
+//            print("passed through: ", receivedTimeStamp)
+//        }
+//
+//        cancellable.store(in: &self.subscriptions)
     }
 }
 
