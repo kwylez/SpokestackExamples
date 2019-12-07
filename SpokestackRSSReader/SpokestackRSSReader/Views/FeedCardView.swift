e0fdb0825ac21cbb9f8ebf6aebdd82399c877b80
Cory D. Wiles <corywiles@icloud.com>
2019-12-06 22:45:43 -0500

    move imageview to separate class

--- SpokestackRSSReader/SpokestackRSSReader/Views/FeedCardView.swift
+++ SpokestackRSSReader/SpokestackRSSReader/Views/FeedCardView.swift
@@ -11,43 +11,6 @@ import SwiftUI
 typealias FeedCardTellMoreCallback = (_ feedItem: RSSFeedItem) -> Void
 typealias FeedCardSeeMoreCallback = (_ url: URL) -> Void
 
-struct ImageView: View {
-    
-    // MARK: Internal (properties)
-    
-    @ObservedObject var imageLoader: RemoteImageController = RemoteImageController()
-    
-    @State var image: Image = Image("default-headline-image")
-    
-    var imageURL: String
-    
-    var body: some View {
-    
-        VStack {
-        
-            self.image
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 72, height: 73)
-
-        }.onReceive(self.imageLoader.$isValidImage, perform: {newValue in
-
-            if newValue {
-                self.image = Image(uiImage: self.imageLoader.image)
-            }
-
-        }).onAppear(perform: {
-            self.imageLoader.fetch(self.imageURL)
-        })
-    }
-    
-    // MARK: Initializers
-    
-    init(_ url: String) {
-        self.imageURL = url
-    }
-}
-
 struct FeedCardView: View {
     
     // MARK: Internal (properties)
